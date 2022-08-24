1412a71125d51e5118e0f713fb215a62b8e92508
Diego FernandezEscobar <fernandezescobardiego@gmail.com>
2022-08-24 18:43:52 -0300

    Top3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-import PokeNav from './componets/header'
+import RenderLogo from './componets/CartWidgets';
+import PokeNav from './componets/navBar'
+import AutoLayoutExample from "./componets/CartWidgets"
+
 function App() {
   return (
     <div className="App">
-    
-      <PokeNav/>
-    
+      {/* // Nav Bar  */}
+       <PokeNav/>
+       {/* // titulo pagina */}
+    <h3 className='top3Titulo'> Enterate del top 3 de esta semana! </h3> 
+     {/* // trae dinamicamente un top 3 pokemons */}
+     <AutoLayoutExample name="Charizard"/>
+     <AutoLayoutExample name="Pikachu"/>
+     <AutoLayoutExample name="Squirtle"/>
+    {/* seleccionalos por clase */}
+
     </div>
   );
 }
